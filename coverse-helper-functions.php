59c831c7494b4026279818a87cc2bc17b233e117
Rado Faletič <radofaletic@gmail.com>
2025-08-22 09:50:09 +1000

    Fixed use of 'pre_set_site_transient_update_plugins' hook.

--- coverse-helper-functions.php
+++ coverse-helper-functions.php
@@ -18,42 +18,40 @@ Update URI:   https://raw.githubusercontent.com/coverseau/coverse-helper-functio
  * Plugin updater handler function.
  * Pings the Github repo that hosts the plugin to check for updates.
  */
-if (!function_exists('coverse_helper_functions_check_for_plugin_update')) {
-  function coverse_helper_functions_check_for_plugin_update($transient) {
-	// If no update transient or transient is empty, return.
-	if (empty($transient->checked)) {
-	  return $transient;
-	}
-	
-	// Plugin slug, path to the main plugin file, and the URL of the update server
-	$plugin_slug = 'coverse-helper-functions/coverse-helper-functions.php';
-	$update_url = 'https://raw.githubusercontent.com/coverseau/coverse-helper-functions/refs/heads/main/update-info.json';
-	
-	// Fetch update information from your server
-	$response = wp_remote_get($update_url);
-	if (is_wp_error($response)) {
-	  return $transient;
-	}
-	
-	// Parse the JSON response (update_info.json must return the latest version details)
-	$update_info = json_decode(wp_remote_retrieve_body($response));
-	
-	// If a new version is available, modify the transient to reflect the update
-	if (version_compare($transient->checked[$plugin_slug], $update_info->new_version, '<')) {
-	  $plugin_data = array(
-		  'slug'        => 'coverse-helper-functions',
-		  'plugin'      => $plugin_slug,
-		  'new_version' => $update_info->new_version,
-		  'url'         => $update_info->url,
-		  'package'     => $update_info->package, // URL of the plugin zip file
-		);
-	  $transient->response[ $plugin_slug ] = (object) $plugin_data;
-	}
-	
-	return $transient;
-  }
-  add_filter('pre_set_site_transient_update_plugins', 'coverse_helper_functions_check_for_plugin_update');
+function coverse_helper_functions_check_for_plugin_update($transient) {
+// If no update transient or transient is empty, return.
+if (empty($transient->checked)) {
+  return $transient;
 }
+
+// Plugin slug, path to the main plugin file, and the URL of the update server
+$plugin_slug = 'coverse-helper-functions/coverse-helper-functions.php';
+$update_url = 'https://raw.githubusercontent.com/coverseau/coverse-helper-functions/refs/heads/main/update-info.json';
+
+// Fetch update information from your server
+$response = wp_remote_get($update_url);
+if (is_wp_error($response)) {
+  return $transient;
+}
+
+// Parse the JSON response (update_info.json must return the latest version details)
+$update_info = json_decode(wp_remote_retrieve_body($response));
+
+// If a new version is available, modify the transient to reflect the update
+if (version_compare($transient->checked[$plugin_slug], $update_info->new_version, '<')) {
+  $plugin_data = array(
+	  'slug'        => 'coverse-helper-functions',
+	  'plugin'      => $plugin_slug,
+	  'new_version' => $update_info->new_version,
+	  'url'         => $update_info->url,
+	  'package'     => $update_info->package, // URL of the plugin zip file
+	);
+  $transient->response[ $plugin_slug ] = (object) $plugin_data;
+}
+
+return $transient;
+}
+add_filter('pre_set_site_transient_update_plugins', 'coverse_helper_functions_check_for_plugin_update');
 
 register_activation_hook( __FILE__, 'coverse_helper_functions_activate');
 function coverse_helper_functions_activate(){
